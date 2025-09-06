ffaebe8d2387068bd908cdb481bc85be65900127
iliaNeustorev <jjnn95@yandex.ru>
2025-09-06 07:37:19 +0300

    add Shop/Product

---
+++ app/Models/Product.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    /** @use HasFactory<\Database\Factories\ProductFactory> */
+    use HasFactory;
+
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+
+    /**
+     *
+     * @return belongsToMany
+     */
+    public function shops()
+    {
+        return $this->belongsToMany(Shop::class);
+    }
+}
---
+++ app/Models/Shop.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Shop extends Model
+{
+    /** @use HasFactory<\Database\Factories\ShopFactory> */
+    use HasFactory;
+
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+
+    /**
+     *
+     * @return belongsToMany
+     */
+    public function products()
+    {
+        return $this->belongsToMany(Product::class);
+    }
+}
